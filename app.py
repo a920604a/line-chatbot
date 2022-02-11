4643dbd99dd438c9bcf8ac76a10160d152e849c6
a920604a <a920604a@gmail.com>
2022-02-11 09:29:09 +0000

    make it better

--- app.py
+++ app.py
@@ -67,6 +67,7 @@ class Bot:
 
     def get_fun(self):
         action_fun = Bot.task_map.get(self.val)
+        factory = None
         if self.val in Bot.task_map:
             factory = Factory
         elif self.val in Bot.task_map:
