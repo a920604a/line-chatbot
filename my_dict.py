be1795ad124ca1afd0fdaa7797dfb011d2e2320e
a920604a <a920604a@gmail.com>
2022-02-11 09:27:18 +0000

    make it better

--- my_dict.py
+++ my_dict.py
@@ -51,40 +51,40 @@ MyDict = defaultdict(
 # class MyDict:
     
 
-    ptt_beauty = 'PTT表特版'
-    ptt_soft_job = '軟體'
-    ptt_tech_job = '工作'
-    gossiping = '即時廢文'
+    # ptt_beauty = 'PTT表特版'
+    # ptt_soft_job = '軟體'
+    # ptt_tech_job = '工作'
+    # gossiping = '即時廢文'
 
-    movie = '近期上映電影'
-    netflix = 'Netflix近期上映'
+    # movie = '近期上映電影'
+    # netflix = 'Netflix近期上映'
 
-    tech_news = '科技快報'
-    tvbs_news = '即時新聞'
+    # tech_news = '科技快報'
+    # tvbs_news = '即時新聞'
 
 
-    oil_price = '油價查詢'
-    rate = '匯率查詢'
+    # oil_price = '油價查詢'
+    # rate = '匯率查詢'
 
-    beverage_50lan = '50嵐菜單'
-    beverage_coco = 'coco菜單'
-    beverage_whitealley = '白巷子菜單'
-    beverage_milkshop = '迷客夏菜單'
-    # beverage_comebuy =  'COMEBUY菜單'
-    beverage_chingshin = '清心菜單'
-    beverage_maculife = '麻古菜單'
-    beverage_presotea = '鮮茶道菜單'
-    beverage_dayungs = '大苑子菜單'
+    # beverage_50lan = '50嵐菜單'
+    # beverage_coco = 'coco菜單'
+    # beverage_whitealley = '白巷子菜單'
+    # beverage_milkshop = '迷客夏菜單'
+    # # beverage_comebuy =  'COMEBUY菜單'
+    # beverage_chingshin = '清心菜單'
+    # beverage_maculife = '麻古菜單'
+    # beverage_presotea = '鮮茶道菜單'
+    # beverage_dayungs = '大苑子菜單'
         
 
-    start_word = 'Hey' 
+    # start_word = 'Hey' 
 
-    ptt_template = '看ptt'
-    news_template = '新聞'
-    movie_template = '電影'
+    # ptt_template = '看ptt'
+    # news_template = '新聞'
+    # movie_template = '電影'
 
-    beverage_template = '飲料菜單'
-    beverage_template1 = '50嵐 coco 白巷子 迷客夏菜單'
+    # beverage_template = '飲料菜單'
+    # beverage_template1 = '50嵐 coco 白巷子 迷客夏菜單'
 
-    beverage_template2 = '清心 麻古 鮮茶道 大苑子菜單'
+    # beverage_template2 = '清心 麻古 鮮茶道 大苑子菜單'
     
