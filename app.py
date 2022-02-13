7cdbd8cf8516f2746bf7a685007accdb4879913f
a920604a <a920604a@gmail.com>
2022-02-13 02:24:13 +0000

    make it better

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import (MessageEvent, TextMessage, TextSendMessage)
 
 from config import Config
-from interface import (gossiping, movie, 
+from interface import (ptt_gossiping, movie, 
                         #   netflix,  # init_task()
                        tvbs_news, oil_price, ptt_beauty, ptt_soft_job, ptt_tech_job, rate, tech_news,
                        beverage_50lan,
